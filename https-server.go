fd94961e542e4a9bb2da8f6bed54a3ca310b04b6
gavinzhou <gavin.zhou@gmail.com>
2018-07-24 10:43:54 +0900

    remove pem

--- https-server.go
+++ https-server.go
@@ -12,7 +12,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	err := http.ListenAndServeTLS(":8081", "dev.test.local+3.pem", "dev.test.local+3-key.pem", nil)
+	err := http.ListenAndServeTLS(":4433", "localhost+3.pem", "localhost+3-key.pem", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
